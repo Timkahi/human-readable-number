9394fa0f31c0bd53861594783da3552423804a8c
Timkahi <91555197+Timkahi@users.noreply.github.com>
2022-02-26 20:53:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,36 @@
+let unitsArray = ['zero','one','two','three','four','five','six','seven','eight','nine'];
+let onedozenArray = ['ten', 'eleven', 'twelve','thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let dozensArray = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+
 module.exports = function toReadable (number) {
-  if (number < 10) {
-        const t = unitsArray[number]
-      return t
-    } else if (number === 20 || number === 30 ||  number === 40 || number === 50 ||number === 60 ||number === 70 ||number === 80 ||number === 90) {
-        const tents = number / 10 
-        
-        return dozensArray[tents]
-    } else if (number === 10 || number === 11 || number ===12 || number ===13 || number === 14 || number === 15 || number ===16 || number ===17 || number ===18 || number === 19 ) {
-        const ten = number % 10
-        return onedozenArray[ten]
-
-    } else if (number < 100) {
-        const first = Math.floor(number/10)
-        const last = number%10
-        return `${dozensArray[first]} ${unitsArray[last]}`
-
-    } else if (number <= 1000) {
-        let dozensArray1 = ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-        const first2 = Math.floor(number/100)
-        const second1 = number%100
-        const second2 = (Math.floor(second1/10))
-        const last1 = number%10
-        console.log(last1)
-        return  `${unitsArray[first2]} hundred ${dozensArray1[second2 -1 ]} ${unitsArray[last1]}`
+    if (number < 10) {
+        return unitsArray[number]
+    } else if (number <= 19) {
+        const idNum = number%10
+        return onedozenArray[idNum]
+    } else if (number > 19 && number <= 99) {
+        const firstNum = Math.floor(number / 10)
+        const lastNum = number % 10
+        if (lastNum === 0) {
+            return dozensArray[firstNum - 1]
+        } else {
+            return `${dozensArray[firstNum - 1]} ${unitsArray[lastNum]}`
+        }
+    } else if (number >= 100 && number < 1000) {
+        const firstThreeNum = Math.floor(number / 100)
+        const midelNum = Math.floor((number % 100) / 10)
+        const lastThreeNum = number % 10
+        const ecspectTwoLastNum = number % 100
+        if (midelNum === 0 && lastThreeNum === 0) {
+            return `${unitsArray[firstThreeNum]} hundred`
+        } else if (midelNum === 0) {
+            return `${unitsArray[firstThreeNum]} hundred ${unitsArray[lastThreeNum]}`
+        } else if (ecspectTwoLastNum >=10 && ecspectTwoLastNum <= 19) {
+            return `${unitsArray[firstThreeNum]} hundred ${onedozenArray[lastThreeNum]}`
+        } else if (ecspectTwoLastNum >10 && ecspectTwoLastNum <= 90 && lastThreeNum === 0) {
+            return `${unitsArray[firstThreeNum]} hundred ${dozensArray[midelNum - 1]}`
+        }
+        return `${unitsArray[firstThreeNum]} hundred ${dozensArray[midelNum - 1]} ${unitsArray[lastThreeNum]}`
     }
 }
